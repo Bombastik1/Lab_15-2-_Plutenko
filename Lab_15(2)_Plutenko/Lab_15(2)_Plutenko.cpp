468bced5c9a5d1e4a8d51b670afee94b27ccdbb4
Bombastik1 <plutenko.ay@gmail.com>
2023-12-29 15:25:03 +0200

    Добавьте файлы проекта.

---
+++ Lab_15(2)_Plutenko/Lab_15(2)_Plutenko.cpp
@@ -0,0 +1,34 @@
+﻿#include <stdio.h> 
+#include <limits.h> 
+#include <locale>  
+int main() {
+    setlocale(LC_ALL, "");
+    int n = 3;
+    int i;
+    int j;
+    int a = 0;
+    int* array = (int*)malloc(n * sizeof(int));
+    printf("Введiть елементи масиву:\n");
+    for (i = 0; i < n; i++) {
+        scanf_s("%d", &array[i]);
+    }
+
+    int* min = array;
+
+    for (i = 1; i < n; i++) {
+        if (array[i] < *min) {
+            min = &array[i];
+        }
+    }
+    printf("Мiнiмальне значення: %d\n", *min);
+    for (int i = 0; i < n; ++i) {
+        for (int j = i + 1; j < n; ++j) {
+            if (array[i] == array[j] && array[i] == *min) {
+                a += 1;
+                break;
+            }
+        }
+    }
+    if (a > 0)
+        printf("В масивi зустрiчається бiльше одного мiнiмального числа");
+}
